c4131cf776cd50b0ed278449dbbdae802e144fb2
ksudyn <ksudyn@student.42madrid.com>
2025-03-27 20:01:37 +0100

    bultins hechos, exit terminado y comprobado a comprobar cuando todo este junto

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -14,6 +14,13 @@
 
 //FUNCIONES DE LIBFT
 
+int	ft_isdigit( int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
 char	*ft_strchr(const char *s, int c)
 {
 	c = (unsigned char)c;
@@ -72,39 +79,42 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 int check_long_number(char *str)
 {
-    unsigned long long result = 0;
-    int sign = 0;
-    int i = 0;
-    int digit = 0;
+    unsigned long long result;
+    int sign;
+    int i;
+    int digit;
 
-    if (str[i] == '-')
-    {
+    result = 0;
+    sign = 0;
+    i = 0;
+    digit = 0;
+    if (str[i++] == '-')
         sign = 1;
-        i++;
-    }
     else if (str[i] == '+')
         i++;
-
     while (str[i])
     {
-        if (!isdigit(str[i]))
+        if (!ft_isdigit(str[i]))
 			return (0);
 		digit = str[i] - '0';
 		if (result > LLONG_MAX / 10
 			|| (result == LLONG_MAX / 10 && digit > (LLONG_MAX % 10) + sign))
 			return (0);
 		result = result * 10 + digit;
-		i++;
+        i++;
     }
     return (1);
 }
 
 int transform_number(char *str, long long *result)
 {
-    unsigned long long digit = 0;
-    int sign = 0;
-    int i = 0;
+    unsigned long long digit;
+    int sign;
+    int i;
 
+    digit = 0;
+    sign = 0;
+    i = 0;
     if (check_long_number(str) == 0)
         return (0);
     if (str[i] == '-')
@@ -119,25 +129,31 @@ int transform_number(char *str, long long *result)
         digit = digit * 10 + (str[i] - '0');
         i++;
     }
-        if (digit > LLONG_MAX + sign)
-            return (0);
-        *result = (long long)digit;
+    if (digit > LLONG_MAX + sign)
+            return (0);   
+    *result = (long long)digit - sign;
     return (1);
 }
 
-
-//por rvisar 
-
-
-int final_numbers(char *str)
+int final_numbers(char *str, long long *result)
 {
     long long number;
+    int i;
 
-    if (*str == '\0')
+    i = 0;
+    if (str[i] == '\0')
         return (0);
 
     if (transform_number(str, &number) == 0)
         return (0);
+
+    if(str[i] == '-')
+    {
+        number = number + 1;
+        *result = -number;
+    }
+    else
+        *result = number;
 
     return (1);
 }
@@ -149,44 +165,102 @@ int multiple_args(char **exit_args, int argc)
 
     trimmed_arg = ft_strtrim(exit_args[1], " \f\r\n\t\v");
     if (!trimmed_arg)
-        return (2); // Error en la asignación de memoria
+        return (2);
 
-    if (final_numbers(trimmed_arg) == 0)
+    if (final_numbers(trimmed_arg, &exit_code) == 0)
     {
         printf("exit\n");
         printf("minishell: exit: %s: numeric argument required\n", exit_args[1]);
         free(trimmed_arg);
         return (2);
     }
-    final_number(trimmed_arg, &exit_code);
+    final_numbers(trimmed_arg, &exit_code);
     free(trimmed_arg);
 
     if (argc > 2)
     {
         printf("exit\n");
         printf("minishell: exit: too many arguments\n");
-        return (-1); // Indica que no se debe salir
+        return (-1);
     }
 
     return (exit_code % 256);
 }
 
-int exit(char **exit_args)
+int ft_exit(char **exit_args)
 {
-    int i = 0;
+    int i;
     int exit_status;
 
+    i = 0;
     while (exit_args[i])
         i++;
 
     if (i == 1)
-        exit(0); // Si no hay argumentos, salir con código 0
+        return (0); // Si no hay argumentos, salir con código 0
 
     exit_status = multiple_args(exit_args, i);
     
     if (exit_status == -1)
         return (1); // No se sale del shell si hay "too many arguments"
 
-    exit(exit_status);
+    return(exit_status);
+}
+
+
+//MAIN CREADO POR CHATGPT PARA COMPROBAR SI FUNCIONA BIEN
+
+// Aquí van tus funciones check_long_number, transform_number, final_numbers, multiple_args, ft_exit
+
+int main(void)
+{
+    char input[1024];
+
+    while (1)
+    {
+        printf("minishell> ");  // Muestra el prompt
+
+        if (fgets(input, sizeof(input), stdin) == NULL) // Lee la entrada del usuario
+            break;
+
+        input[strcspn(input, "\n")] = '\0';  // Elimina el salto de línea al final
+
+        // Dividir el input en palabras para tratar los argumentos
+        char *args[10];  // Suponemos que el número máximo de argumentos es 10
+        int i = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL && i < 10)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;  // Termina la lista de argumentos con NULL
+
+        // Comprobar si el comando es "exit"
+        if (strcmp(args[0], "exit") == 0)
+        {
+            int exit_status = multiple_args(args, i);
+            if (exit_status == 0)
+            {
+                printf("Exiting with code 0\n");
+                break;  // Salir del programa con código de salida 0
+            }
+            else if (exit_status == 1)
+            {
+                printf("Too many arguments. Not exiting.\n");
+            }
+            else if (exit_status == 2)
+            {
+                printf("Invalid argument. Exiting with error code 2.\n");
+            }
+            else
+            {
+                printf("Exiting with code %d\n", exit_status);
+                break;  // Salir del programa con el código de salida calculado
+            }
+        }
+    }
+
+    return 0;
 }
 
